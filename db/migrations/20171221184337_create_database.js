8e2f26a4144bb4c64337375e0e71b1f41ee57ee7
Mandy <mandycheang135@gmail.com>
2017-12-21 20:38:59 +0000

    Add resource_tag table in migration

--- db/migrations/20171221184337_create_database.js
+++ db/migrations/20171221184337_create_database.js
@@ -13,15 +13,18 @@ exports.up = function(knex, Promise) {
       table.increments();
       table.foreign('user_id').references('user.id');
       table.foreign('resource_id').references('resources.id');
-      table.integer('like')
-
+      table.integer('like');
     }),
     knex.schema.createTable('rating', function (table) {
       table.increments();
-
+      table.foreign('user_id').references('user.id');
+      table.foreign('resource_id').references('resources.id');
+      table.integer('value');
     }),
     knex.schema.createTable('resources_tag', function (table) {
       table.increments();
+      table.foreign('tag_id').references('tag.id');
+      table.foreign('resource_id').references('resources.id');
 
     }),
     knex.schema.createTable('tag', function (table) {
