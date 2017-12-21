5ab351b38256f11d98c1fe1916dabc5c1c453f16
Mandy <mandycheang135@gmail.com>
2017-12-21 16:15:32 +0000

    Add div to fix spacing

--- public/scripts/resources.js
+++ public/scripts/resources.js
@@ -9,11 +9,13 @@ $(() => {
       let urls = resource.url;
       console.log(title)
       $("#resources-row").append(`
-        <div class='border border-dark rounded p-4 content col-4 text-center'>
-          <div><a href='http://${urls}'><img src='http://fillmurray.com/200/200'></a></div>
-          <div>${title}</div>
-          <div>${description}</div>
-          <div>comment</div>
+        <div class='content col-4 text-center'>
+          <div class='border border-dark rounded m-1'>
+            <div><a href='http://${urls}'><img src='http://fillmurray.com/200/200'></a></div>
+            <div>${title}</div>
+            <div>${description}</div>
+            <div>comment</div>
+          </div>
         </div>
         `);
     }
