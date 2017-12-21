e8372bec49eb4fb57749b13fa567116625d2e106
Mandy <mandycheang135@gmail.com>
2017-12-21 20:32:48 +0000

    Add like in database

---
+++ db/migrations/20171221184337_create_database.js
@@ -0,0 +1,40 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('comment', function (table) {
+      table.increments();
+      table.foreign('user_id').references('user.id');
+      table.foreign('resource_id').references('resources.id');
+      table.timestamp('created_at');
+      table.timestamp('updated_at');
+      table.string('content');
+    }),
+    knex.schema.createTable('like', function (table) {
+      table.increments();
+      table.foreign('user_id').references('user.id');
+      table.foreign('resource_id').references('resources.id');
+      table.integer('like')
+
+    }),
+    knex.schema.createTable('rating', function (table) {
+      table.increments();
+
+    }),
+    knex.schema.createTable('resources_tag', function (table) {
+      table.increments();
+
+    }),
+    knex.schema.createTable('tag', function (table) {
+      table.increments();
+
+    }),
+
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('resources')
+  ])
+};
+
