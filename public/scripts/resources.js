d1ad3caefc9a42d89de6ddd9285ddf20a60bfbb8
Bryan <bryanlampert@gmail.com>
2017-12-22 20:29:04 -0500

    Resources Buttons
    Fixed the resources buttons to toggle based on what's clicked

--- public/scripts/resources.js
+++ public/scripts/resources.js
@@ -1,28 +1,24 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/resources"
-  }).done((resources) => {
-    for(resource of resources) {
-      let title = resource.title;
-      let description = resource.description;
-      let urls = resource.url;
-      $("#resources-row").append(`
-        <div class='content col-4 text-center'>
-          <div class='border border-dark rounded m-1'>
-            <div><a href='http://${urls}'><img src='http://fillmurray.com/200/200'></a></div>
-            <div>${title}</div>
-            <div>${description}</div>
-            <div>comment</div>
-            <i class="far fa-heart"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-          </div>
-        </div>
-        `);
+$(document).ready(function() {
+
+  $(".filters").click(function () {
+    // if ("#all" ==)
+    if ($(this).text() === "All") {
+      $(".filters").removeClass("active");
+    } else {
+      $("#all").removeClass("active");
     }
-  });;
+    $(this).toggleClass("active");
+
+    // TO DO:
+    // Filter the resources that are shown when a specific button is clicked.
+
+  });
+
+
+
+
+
+
+
+
 });
