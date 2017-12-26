43300626978c0767ecfae8df23c0eb50bb684f16
Mandy <mandycheang135@gmail.com>
2017-12-26 04:40:11 +0000

    Finish filtering and refactor filtering functions

--- public/scripts/resources.js
+++ public/scripts/resources.js
@@ -3,10 +3,21 @@ function renderAllResources() {
     $.ajax({
       method: "GET",
       url: "/api/resources"
-    }).done((resources) => {
+    }).then((resources) => {
       createAndAppendResource(resources);
     })
   );
+}
+
+function renderFilteredResources(resourceCategories) {
+  $(() => {
+    $.ajax({
+      method: "GET",
+      url: `/api/resources?types=${resourceCategories}`
+    }).then((resources) => {
+      createAndAppendResource(resources);
+    });
+  });
 }
 
 function createAndAppendResource(resources) {
@@ -35,109 +46,50 @@ function createResourceElement(resource) {
   );
 }
 
-function removeResources() {
+function removeResources(resourcesToRender, cb) {
   $(() => {
     $.ajax({
       method: "GET",
       url: "/api/resources"
-    }).done((resources) => {
+    }).then((resources) => {
       resources.forEach(function() {
         $('.each-resource').remove();
       });
+      cb(resourcesToRender);
     });
   });
 }
 
-function removeFilterElement(array, elm) {
-  const index = array.indexOf(elm);
-  array.splice(index,1);
-}
-
-function toggleBtnState (btn) {
+function toggleBtnActive (btn) {
   $(btn).click(function() {
     $(this).toggleClass("active");
   });
 }
 
-let filterTerms = [];
-let urlParams = JSON.stringify({ data: filterTerms });
+function loadAllResources(filterBtn) {
+  if(!$("filterBtn").hasClass("active")) {
+    renderAllResources();
+  }
+}
+
+function renderResourcesOnClick(filterBtn, activeFilterBtns) {
+  $(filterBtn).click(function() {
+
+    let resourcesToFilter = [];
+    for (btn of $(activeFilterBtns)) {
+      resourcesToFilter.push(btn.id)
+    }
+    let resourceCategories = JSON.stringify({ data: resourcesToFilter });
+    removeResources(resourceCategories, renderFilteredResources);
+
+    if (resourcesToFilter.length === 0) {
+      renderAllResources();
+    }
+  });
+}
 
 $(document).ready(() => {
-
-  toggleBtnState("#all");
-  toggleBtnState("#blog");
-  toggleBtnState("#tutorial");
-  toggleBtnState("#book");
-  toggleBtnState("#article");
-  toggleBtnState("#video");
-
-
-  // $(".filters").click(function() {
-
-  //   if ($(this).text() === "All") {
-  //     filterTerms.splice(0, filterTerms.length); // removes all elements in filterTerms
-  //     $(".filters").removeClass("active"); // removes class 'active' from everything EXCEPT all because all is active on default
-  //     $(() => {
-  //         $.ajax({
-  //           method: "GET",
-  //           url: "/api/resources"
-  //         }).done((resources) => {
-  //           resources.forEach(function() {
-  //             $('.each-resource').remove();
-  //           });
-  //           renderAllResources();
-  //         });
-  //       })
-
-  //   } else {
-  //     $("#all").removeClass("active"); // makes "All" button inactive
-
-  //     if ($(this).text() === "Blog") {
-
-  //       $(this).toggleClass("active");
-
-  //       console.log("State of blog: " + $(this).hasClass("active"))
-
-  //       if ($(this).hasClass("active")) {
-  //         filterTerms.push("blog");
-  //         let urlParams = JSON.stringify({ data: filterTerms });
-  //         console.log("Url params inside Tutorial:" + urlParams)
-
-  //         function renderFilterItems() {
-  //           $(() => {
-  //             $.ajax({
-  //               method: "GET",
-  //               url: `/api/resources?types=${urlParams}`
-  //             }).done((resources) => {
-  //               createAndAppendResource(resources);
-  //             });
-  //           });
-  //         }
-
-  //         // remove all resources, then render only the blog resources
-  //         $(() => {
-  //           $.ajax({
-  //             method: "GET",
-  //             url: "/api/resources"
-  //           }).done((resources) => {
-  //             resources.forEach(function() {
-  //               $('.each-resource').remove();
-  //             });
-  //             renderFilterItems();
-  //           });
-  //         })
-  //       } else {
-  //         // console.log('should put inactive here')
-  //         removeFilterElement(filterTerms,'blog');
-  //         // console.log(filterTerms)
-  //         removeResources();
-  //       }
-  //     }
-  //   } //else
-  // }) // on click for filters
-
-  // renderAllResources();
-
-}); // document ready
-
-
+  toggleBtnActive(".filter-btn");
+  loadAllResources(".filter-btn");
+  renderResourcesOnClick(".filter-btn", ".filter-btn.active");
+});
