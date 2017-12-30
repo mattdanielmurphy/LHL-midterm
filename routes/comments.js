3cceee574a816215f20a3e143a5ecad17208a066
Mandy <mandycheang135@gmail.com>
2017-12-30 18:19:56 +0000

    Add comment to databse with correct resource id

--- routes/comments.js
+++ routes/comments.js
@@ -6,18 +6,25 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.post("/", (req, res) => {
-    console.log(req.body.text, "<--req.body.text");
-    console.log(req.session.username, "<--req.session.username")
-    knex('comments')
-      .insert({
-        // user_id: req.session.id,
-        content: req.body.text
-      })
+    // console.log(req.body.text, "<-- text area content");
+    // console.log(req.session.id, "<-- user id");
+    // console.log(req.body.url, "<--backend url")
+    knex('resources')
+      .select('id')
+      .where('url',req.body.url)
       .then((result) => {
+        knex('comments')
+          .insert({
+            user_id: req.session.id,
+            resource_id: result[0].id,
+            content: req.body.text,
+          })
+          .then((result) => {
+          })
+          .catch((err) => {
+            console.log(err)
+          });
       })
-      .catch((err) => {
-        console.log(err)
-      });
   }); //router.get end
 
   return router;
