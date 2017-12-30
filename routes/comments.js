f17428f1294407fc9dc753ec0f05b0a3ec641a96
Mandy <mandycheang135@gmail.com>
2017-12-30 16:06:51 +0000

    Require username from cookie

--- routes/comments.js
+++ routes/comments.js
@@ -7,12 +7,16 @@ module.exports = (knex) => {
 
   router.post("/", (req, res) => {
     console.log(req.body.text, "<--req.body.text");
+    console.log(req.session.username, "<--req.session.username")
     knex('comments')
       .insert({
         // user_id: req.session.id,
         content: req.body.text
       })
       .then((result) => {
+      })
+      .catch((err) => {
+        console.log(err)
       });
   }); //router.get end
 
